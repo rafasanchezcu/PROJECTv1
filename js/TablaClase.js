6555ce8eb3bb95d22247b9ef5114b8a1f5f30d7a
rafasanchezcu <rafaa.sanchez@hotmail.com>
2016-12-13 23:01:23 -0500

    MONITOREO EN TABLA 98%
    1- Se agrega la funcion cellClass funcional para que compare las horas
    reales con las horas estimadas y retorne un color dependiendo de la
    condicion.
    
    2-Se agrega la funcion cellClicked en el metodo de cellTemplate de ui
    grid para mostrar en un alert las horas estimadas en cada celda de las
    horas reales.
    
    3-Falta idicaciones de colores en el html

--- js/TablaClase.js
+++ js/TablaClase.js
@@ -5,12 +5,15 @@ var rutaSeleccionada = window.location.search.split("=")[1];
 
 app.controller('MainCtrl', ['$scope', '$http','i18nService', function ($scope, $http, i18nService ) {
 
-  
- $scope.deleteRow = function(row) {
+ /*$scope.deleteRow = function(row) {
     var index = $scope.gridOptions.data.indexOf(row.entity);  
     $scope.gridOptions.data.splice(index, 1);
   };
+*/
 
+ $scope.cellClicked = function (row, col){
+  alert('Hora LLegada Estimada: '+row.entity.HorarioEstimado.ST2 +'\n'+  'Hora LLegada Real: '+ row.entity.HorarioReal.ST2);
+  };
   
     
     $scope.gridOptions = {
@@ -20,32 +23,68 @@ app.controller('MainCtrl', ['$scope', '$http','i18nService', function ($scope, $
      showGridFooter:true,
     columnDefs: [
       
-      {name: 'Eliminar', width: '7%', cellTemplate: '<button class="btn primary" ng-click="grid.appScope.deleteRow(row)">Eliminar</button>'},
+      // {name: 'Eliminar', width: '7%', cellTemplate: '<button class="btn primary" ng-click="grid.appScope.deleteRow(row)">Eliminar</button>'},
       {field: 'Clave', width: '6%'},
-      {field: 'Conductor', width: '8%'},
-      {field: 'Placa', width: '7%'},
-      {field: 'Ruta', width: '6%'},
+      {field: 'Bus', width: '8%'},
+      {field: 'Conductor', width: '9%'},
+      {field: 'Recorrido',  name:'Ruta', width: '6%'},
+      {field: 'EstaTerminado',  name:'Estado', width: '7%', 
+    cellClass: function(grid, row, col, rowRenderIndex, colRenderIndex) {
+          if (grid.getCellValue(row,col) == true) {
+            return 'red1';
+          }
+          else {
+            return 'green';
+          }
+        }
+      },
       {field: 'HoraSalidaEstimada', name:'H.S.E', width: '7%'},
       {field: 'HoraSalidaReal', name:'H.S.R', width: '7%'},
-      {field: 'HorarioEstimado.ST1', name:'H.E-est1', width: '8%'},
-      {field: 'HorarioReal.ST1', name:'H.R-est1', width: '8%',
+      {field: 'HorarioEstimado.ST1', name:'H.E-est1', visible:false, width: '8%'},
+      {field: 'HorarioReal.ST1', name:'HR-est1', width: '8%',
         cellClass: function(grid, row, col, rowRenderIndex, colRenderIndex) {
-          if (grid.getCellValue(row,col) < 'HorarioEstimado.ST1') {
+          if (grid.getCellValue(row,col) > row.entity.HorarioEstimado.ST1) {
             return 'red';
           }
         }
       },
-      {field: 'HorarioEstimado.ST2', name:'H.E-est2', width: '8%'},
-      {field: 'HorarioReal.ST2', name:'H.R-est2', width: '8%'},
-      {field: 'HorarioEstimado.ST3', name:'H.E-est3', width: '8%'},
-      {field: 'HorarioReal.ST3', name:'H.R-est3', width: '8%'},
-      {field: 'HorarioEstimado.ST4', name:'H.E-est4', width: '8%'},
-      {field: 'HorarioReal.ST4', name:'H.R-est4', width: '8%'},
-      {field: 'HorarioEstimado.ST5', name:'H.E-est5', width: '8%'},
-      {field: 'HorarioReal.ST5', name:'H.R-est5', width: '8%'},
-      {field: 'HorarioReal.ST6', name:'H.R-est6', width: '8%'}
+      {field: 'HorarioEstimado.ST2', name:'H.E-est2', visible: false, width: '8%'},
+      {field: 'HorarioReal.ST2', name:'HR-est2', width: '8%',
+      cellClass: function(grid, row, col, rowRenderIndex, colRenderIndex) {
+          if (grid.getCellValue(row,col) > row.entity.HorarioEstimado.ST2) {
+            return 'red';
+          }
+        }, 
+        cellTemplate:'<div ng-click="grid.appScope.cellClicked(row,col)"  class="ui-grid-cell-contents" title="TOOLTIP">{{COL_FIELD CUSTOM_FILTERS}}</div>'
+      },
+      {field: 'HorarioEstimado.ST3', name:'H.E-est3', visible: false, width: '8%'},
+      {field: 'HorarioReal.ST3', name:'HR-est3', width: '8%',
+    cellClass: function(grid, row, col, rowRenderIndex, colRenderIndex) {
+          if (grid.getCellValue(row,col) > row.entity.HorarioEstimado.ST3) {
+            return 'red';
+          }
+        }
+      },
+      {field: 'HorarioEstimado.ST4', name:'H.E-est4', visible: false, width: '8%'},
+      {field: 'HorarioReal.ST4', name:'HR-est4', width: '8%', 
+    cellClass: function(grid, row, col, rowRenderIndex, colRenderIndex) {
+          if (grid.getCellValue(row,col) > row.entity.HorarioEstimado.ST1) {
+            return 'red';
+          }
+        }
+      },
+      {field: 'HorarioEstimado.ST5', name:'H.E-est5', visible: false, width: '8%'},
+      {field: 'HorarioReal.ST5', name:'HR-est5', width: '8%', 
+    cellClass: function(grid, row, col, rowRenderIndex, colRenderIndex) {
+          if (grid.getCellValue(row,col) > row.entity.HorarioEstimado.ST1) {
+            return 'red';
+          }
+        }
+      }
 
     ],
+    maxWidth:9,
+    minWidth:6,
     rowHeight: 35,
     exporterMenuCsv: false,
     enableGridMenu: true,
